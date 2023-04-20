05a09696afa32eccdf95b2a786ceeb90e39b9c6d
Micke Nordin <kano@sunet.se>
2023-04-20 16:22:29 +0200

    Remove template files

--- stepupauth/lib/Service/NoteNotFound.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-// SPDX-FileCopyrightText: Mikael Nordin <kano@sunet.se>
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-namespace OCA\StepUpAuth\Service;
-
-class NoteNotFound extends \Exception {
-}
